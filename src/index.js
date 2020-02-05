37e58170ee7642312f989841261276a99d30af2c
hsisco <hysensisco@gmail.com>
2020-02-04 20:39:52 -0800

    reducer index: appReducer + initialState

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
-import { appReducer as reducer } from './reducers';
+import { appReducer as reducer } from './reducer';
 import { Provider } from 'react-redux';
 import App from './App';
 
