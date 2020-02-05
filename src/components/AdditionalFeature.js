d82f7753a965f7f081bade5630b5516d32a1ae9b
hsisco <hysensisco@gmail.com>
2020-02-04 21:57:35 -0800

    Update AdditionalFeature.js
    handleAdd

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 const AdditionalFeature = props => {
+
+  const handleAdd = () => {
+    props.addFeature(props.feature);
+  }
+
   return (
     <li>
-      {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button 
+      className="button"
+      onClick={handleAdd}>+</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
